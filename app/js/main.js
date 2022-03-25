4def7ab672ededf6aa3fdcab3378d44b6bc9e2d7
luciphoree <luciphoree@gmail.com>
2022-03-25 19:28:54 +0300

    next2

--- app/js/main.js
+++ app/js/main.js
@@ -29,70 +29,105 @@
 
 
 
-let tvLook;
-let personalMovieDB = {
-    count: {},
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
+// let tvLook;
+// let personalMovieDB = {
+//     count: {},
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
 
-start()
-questions()
-showMyDB(personalMovieDB.privat)
-writeYourGenres()
-function start() {
-    tvLook = +prompt("Сколько фильмов посмотрели?")
-    while (tvLook == "" || tvLook == null || isNaN(tvLook)) {
-        tvLook = prompt("Сколько фильмов посмотрели?")
-    }
-    if (tvLook < 10) {
-        alert('МАло')
-    } else if (tvLook > 10 && tvLook < 30) {
-        alert('средне')
-    } else {
-        alert('много')
+// start()
+// questions()
+// showMyDB(personalMovieDB.privat)
+// writeYourGenres()
+// function start() {
+//     tvLook = +prompt("Сколько фильмов посмотрели?")
+//     while (tvLook == "" || tvLook == null || isNaN(tvLook)) {
+//         tvLook = prompt("Сколько фильмов посмотрели?")
+//     }
+//     if (tvLook < 10) {
+//         alert('МАло')
+//     } else if (tvLook > 10 && tvLook < 30) {
+//         alert('средне')
+//     } else {
+//         alert('много')
 
+//     }
+// }
+// function showMyDB(hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB)
+//     }
+
+// }
+
+// function writeYourGenres() {
+//     for (i = 1; i <= 3; i++) {
+//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+//     }
+// }
+
+
+
+// function questions() {
+//     for (i = 0; i < 2; i++) {
+//         const filmLastQ = prompt("Последний фильм?");
+//         const filmLastQReview = +prompt("Оценка фильма?");
+//         if (filmLastQ != "" && filmLastQReview != "" && filmLastQ != null && filmLastQReview != null && filmLastQ.length < 10) {
+//             personalMovieDB.movies[filmLastQ] = filmLastQReview
+//         } else {
+//             i--
+//         }
+//     }
+// }
+
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учусь!: ${lang}`);
+//     callback();
+// }                                                  // в виде аргумента передаётся другая функция которая и есть callback
+
+
+// function done() {
+//     console.log('Yes')
+// }
+
+
+
+// learnJS('хорошо', function () {
+//     console.log('Yes')
+// });
+
+// learnJS('js', done)
+
+
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
     }
 }
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB)
-    }
-
-}
-
-function writeYourGenres() {
-    for (i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
 
 
 
-function questions() {
-    for (i = 0; i < 2; i++) {
-        const filmLastQ = prompt("Последний фильм?");
-        const filmLastQReview = +prompt("Оценка фильма?");
-        if (filmLastQ != "" && filmLastQReview != "" && filmLastQ != null && filmLastQReview != null && filmLastQ.length < 10) {
-            personalMovieDB.movies[filmLastQ] = filmLastQReview
-        } else {
-            i--
+let counter = 0;
+
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let keyy in options[key]) {
+            console.log(`Свойства ${keyy} имеет значение ${options[key][keyy]}`)
+            counter++
         }
+    } else {
+        console.log(`Свойства ${key} имеет значение ${options[key]}`)
+        counter++
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(options)
+console.log(counter)
